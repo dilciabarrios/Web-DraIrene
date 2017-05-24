9e341e0b0073bbd79e643499092243e1f475950e
Dilcia Barrios <dilciabarrios@gmail.com>
2017-05-24 16:57:11 -0430

    Cambios en estructura de blog
    Cambios en estructura de blog del websita para configuracion de
    paginacion

--- blog.php
+++ blog.php
@@ -24,19 +24,34 @@ include("admin/resources/includes/database.php");
                         </h2>
                     <hr>
                     <br>
-                        <?
-                        $result=query("SELECT * FROM blog");
-                        while($row = fetch_array($result)) {
+                    <?
 
-                       echo' <a href="blogs.php?id='.$row['id'].'" class="list-group-item">
-                            <p class="list-group-text">'.$row['fecha'].'</p>
-                            <p>'.$row['titulo'].'</p>
-                        </a>';
-                         } 
-                        echo' 
-                        <br>
-                        <a href="" class="btn btn-default btn-lg">Read More</a>';
-                        ?>   
+					            $num_rows= 1; // definimos variable de cuantos resultados se van a mostrar por pagina
+
+					            if (isset($_GET['pagina'])){ //examinamos en este caso nuestra pagina a mostrar
+					                $pagina = $_GET['pagina'];
+					            }
+					            else{
+					                $pagina= 1; // sentencia else para definir nuestro valor de inicio
+					            }
+
+					              $ubicacion = ($pagina -1)* $num_rows; //calculamos el desplazamiento para nuestra consulta
+					              //ubicacion de nuestro nro de pagina primer caso ubicacion= 2-1*1 = 1 pagina / 1(elemento a mostrar)
+					              //la pagina 1 va hacer nuestro inicio
+
+                        $result=query("SELECT * FROM blog ORDER BY id DESC LIMIT 6");
+                        for ($i=1; $row=fetch_array($result); $i++) { 
+
+	                       echo' <a href="blog.php?pagina='.$i.'" class="list-group-item">
+	                            <p class="list-group-text">'.$row['fecha'].'</p>
+	                            <p>'.$row['titulo'].'</p>
+	                        </a>';
+	                         } 
+	                        echo' 
+	                        <br>
+	                        <a href="" class="btn btn-default btn-lg">Read More</a>';
+
+                   ?>   
 
 
                     <hr>
@@ -55,9 +70,11 @@ include("admin/resources/includes/database.php");
                     </a> 
             </aside>
             <?
-                $result=query("SELECT blog.id as id_blog, blog.titulo AS titulo_blog, blog.contenido AS contenido_blog, blog.imagen AS imagen_blog, blog.autor AS autor_blog, blog.id AS id_blog, categorias.nombre AS nombre_categoria FROM blog INNER JOIN categorias ON categorias.id_categoria = blog.id_categoria ORDER BY id DESC LIMIT 3");
-                $contador= 1;
-                while($row = fetch_array($result)) 
+
+                $result=query("SELECT blog.id as id_blog, blog.titulo AS titulo_blog, blog.contenido AS contenido_blog, blog.imagen AS imagen_blog, blog.autor AS autor_blog, blog.id AS id_blog, categorias.nombre AS nombre_categoria FROM blog INNER JOIN categorias ON categorias.id_categoria = blog.id_categoria ORDER BY id DESC LIMIT $ubicacion, $num_rows");
+
+
+                for ($i=1; $row=fetch_array($result); $i++) 
                 { echo '
                 <section class="posts col-md-9">
 
@@ -72,31 +89,46 @@ include("admin/resources/includes/database.php");
                         </h2>
                         <p>'.$row['contenido_blog'].'</p>
 
-                        <a href="blogs.php?id='.$row['id_blog'].'" class="btn btn-default btn-lg">Read More</a>
+                        <a href="blog.php?pagina='.$i.'" class="btn btn-default btn-lg">Read More</a>
                         <hr>
-                    </div> 
-                </section>';}?> 
-       </div>
-            <nav>
-                <div class="center-block" align="center">
-                    <ul class="pagination">
-                        <li class="disabled"><a href="#">&laquo;<span class="sr-only">Anterior</span></a></li>
-                        <?
-                        $result= query("SELECT * FROM blog");
-                            for($i=0; $row = fetch_array($result); $i++){
-                            	if($i=0){
-                            		echo '<li class="active"><a href="blog.php">0</a></li>';
-                            	}
-                                else{
-                            		echo '<li><a href="blogs.php?id='.$row['id'].'">'.$i.'</a></li>';
-                                }
-                            }
+                    </div>  ';}?>
 
-                        ?>
-                        <li class="disabled"><a href="#">&raquo;<span class="sr-only">Siguiente</span></a></li>
-                    </ul>
-                </div>
+             <nav>
+                <div class="center-block" align="center">
+                    <ul class="pagination"> 
+    
+                    <?
+	                  			
+	                        $result= query("SELECT * FROM blog");
+	                        $total_filas= num_rows($result); // total nro de filas de mi tabla blog
+	                        $total_pagina=ceil($total_filas/$num_rows); //total de paginas a mostrar ceil para redondear
+
+	                        //anadimos valores a nuestros botones
+	                        $prev = $pagina -1;
+	                        $next = $pagina +1;
+
+	                        if ($prev >0){
+	                        	echo "<li><a href='blog.php?pagina=".$prev."' title='Anterior'>".'<<'."</a></li>";
+
+	                          echo '<li class="enable"><a href="blog.php?pagina='.$prev.'">&laquo;<span class="sr-only">Anterior</span></a></li>';
+	                        }
+	                        //mostrar el resultado de paginas de nuestra base de datos
+	                        for ($i=1; $i <$total_pagina; $i++) { 
+	                            echo "<li><a href='blog.php?pagina=$i' title='Abrir pagina'>" . '&nbsp;&nbsp' .$i."</a></li>";
+	                        }
+
+	                        if ($pagina < $total_filas){
+                             echo '<li class="enable"><a href="blog.php?pagina='.$next.'">&raquo;<span class="sr-only">Siguiente</span></a></li>';
+                             echo "<li><a href='blog.php?pagina=$total_pagina' title='Ir a la ultima pagina'>".'>>'."</a></li>";
+	                        }
+	                    ?>
+                    
+                  </ul>
             </nav>
+        </section>'
+
+       </div>
+
     </div>
 </section>
 
@@ -105,4 +137,3 @@ include("admin/resources/includes/database.php");
 </body>
 
 </html>
-
---
+++ blog.sql
@@ -0,0 +1,53 @@
+
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 24-05-2017 a las 21:24:08
+-- Versión del servidor: 10.1.22-MariaDB
+-- Versión de PHP: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `u793779998_bg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `blog`
+--
+
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(150) DEFAULT NULL,
+  `contenido` longtext,
+  `fecha` date DEFAULT NULL,
+  `imagen` varchar(250) CHARACTER SET latin1 DEFAULT NULL,
+  `id_categoria` varchar(250) DEFAULT NULL,
+  `autor` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=44 ;
+
+--
+-- Volcado de datos para la tabla `blog`
+--
+
+INSERT INTO `blog` (`id`, `titulo`, `contenido`, `fecha`, `imagen`, `id_categoria`, `autor`) VALUES
+(6, 'Lorem Ipsum 1', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.\r\nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', '2017-05-19', 'slide-1.jpg', '1', 'Administrador'),
+(39, 'Lorem Ipsum 2', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.\r\nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', '2017-05-22', 'slide-2.jpg', '1', 'Administrador'),
+(40, 'Lorem Ipsum 3', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.\r\nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', '2017-05-22', 'slide-3.jpg', '1', 'Administrador'),
+(41, 'Lorem Ipsum 4', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.\r\nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', '2017-05-22', 'slide-1.jpg', '1', 'Administrador');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
